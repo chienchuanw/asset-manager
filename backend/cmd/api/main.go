f3977dfffaffa8b87f0767491d5d9747a0b2c1e7
chienchuanw <chienchuanwww@gmail.com>
2025-10-23 00:33:16 +0800

    fix: resolve cors issue from backend

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// Create a new Gin router
 	router := gin.Default()
+
+	router.Use(cors.Default())
 
 	// Define a health check endpoint
 	router.GET("/health", func(c *gin.Context) {
