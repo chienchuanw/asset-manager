59af8694303988d2799e3dcc218934868c39bcf3
chienchuanw <chienchuanwww@gmail.com>
2025-10-23 00:09:42 +0800

    feat: config backend roughly

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Asset Manager API Server Starting...")
+}
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/chienchuanw/asset-manager
+
+go 1.25.3
